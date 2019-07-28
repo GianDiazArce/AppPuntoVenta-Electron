d911f047257771695852d60c0e5a1c3ee95ca2fe
GianDiazArce <giandiazarce@gmail.com>
2019-07-28 04:54:16 -0500

    Iniciando la aplicacion con electron... y agregando index.html

---
+++ src/index.js
@@ -0,0 +1,101 @@
+const { app, BrowserWindow, Menu } = require('electron');
+
+
+const url = require('url');
+const path = require('path');
+
+if(process.env.NODE_ENV !== 'production'){
+    require('electron-reload')(__dirname,{
+        electron: path.join(__dirname, '../node_modules', '.bin', 'electron')
+    });
+}
+
+let mainWindow;
+let tallasWin;
+
+function createWindow(){
+    // Iniciar la ventana con tamaños
+    let ancho = 1024;
+    let alto = 720;
+    mainWindow = new BrowserWindow({
+        width: ancho,
+        height: alto
+    });
+
+    // Cargar el index.html
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'views/index.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+
+    let mainMenu = Menu.buildFromTemplate(templateMenu);
+    Menu.setApplicationMenu(mainMenu);
+
+    // Abre las herramientas de desarrollo (DevTools).
+    mainWindow.webContents.openDevTools()
+
+    // Cuando la ventana es cerrada
+    mainWindow.on('closed', () => {
+        // Elimina la referencia al objeto window, normalmente  guardarías las ventanas
+        // en un vector si tu aplicación soporta múltiples ventanas, este es el momento
+        // en el que deberías borrar el elemento correspondiente.
+        win = null
+    });    
+}
+
+// Este método será llamado cuando Electron haya terminado
+// la inicialización y esté listo para crear ventanas del navegador.
+// Algunas APIs pueden usarse sólo después de que este evento ocurra.
+app.on('ready', createWindow)
+
+// Sal cuando todas las ventanas hayan sido cerradas.
+app.on('window-all-closed', () => {
+    // En macOS es común para las aplicaciones y sus barras de menú
+    // que estén activas hasta que el usuario salga explicitamente con Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+})
+  
+app.on('activate', () => {
+    // En macOS es común volver a crear una ventana en la aplicación cuando el
+    // icono del dock es clicado y no hay otras ventanas abiertas.
+    if (win === null) {
+        createWindow()
+    }
+})
+  
+// En este archivo puedes incluir el resto del código del proceso principal de
+// tu aplicación. También puedes ponerlos en archivos separados y requerirlos aquí.
+
+//Template del menu
+const templateMenu = [
+    {
+        label: 'Archivo',
+        submenu: [
+            {
+                label: 'Nueva Talla',
+                accelerator: 'Ctrl + N',
+                click() {
+                    tallasWindow()
+                }
+            }
+        ]
+    }
+];
+
+function tallasWindow(){
+    tallasWin = new BrowserWindow({
+        width: 400,
+        height: 330,
+        title: 'Tallas'
+    });
+    tallasWin.setMenu(null);
+    tallasWin.loadURL(url.format({
+        pathname: path.join(__dirname, 'views/tallas.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+}
+
